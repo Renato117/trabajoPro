cefcb6756a87d90bfdb3c116b72110195f0e4760
Renato <jhoni.dragon1234@gmail.com>
2025-08-11 09:53:13 -0600

    cambio2

--- CursosDjango/CursosDjango/settings.py
+++ CursosDjango/CursosDjango/settings.py
@@ -71,13 +71,8 @@ WSGI_APPLICATION = 'CursosDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cursos',
-        'USER': 'root',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '3307',
-        'OPTIONS': {'charset': 'utf8mb4'},
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
